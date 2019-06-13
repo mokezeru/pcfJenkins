98eb3101f5bc401e7802ea77e3713e9f7eb595f6
P2883280 <C-MequannintMunye.Zeru@charter.com>
2019-06-13 11:40:10 -0500

    polishing

--- src/main/java/com/charter/pcfJenkins/controller/HelloController.java
+++ src/main/java/com/charter/pcfJenkins/controller/HelloController.java
@@ -1,12 +1,14 @@
+package com.charter.pcfJenkins.controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/hello")
+@RequestMapping(value = "/api")
 public class HelloController {
 
-    @GetMapping
+    @GetMapping("/hello")
     public String hello(){
         return "Hello Moke";
     }
